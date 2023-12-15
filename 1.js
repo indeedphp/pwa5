8d81e68b3af603eba3baa7b8544b42910000edc5
indeedphp <ntsiti@gmail.com>
2023-12-16 01:46:58 +0600

    ввв

--- 1.js
+++ 1.js
@@ -44,3 +44,25 @@ self.addEventListener('install', async event => {
 self.addEventListener('activate', event => {
 console.log('Service worker activate')
 })
+
+
+var CACHE_NAME = 'my-web-app-cache';
+var urlsToCache = [
+  '/',
+  '/styles/main.css',
+  '/scripts/app.js',
+  '/scripts/lib.js'
+];
+
+self.addEventListener('install', function(event) {
+  // event.waitUntil принимает промис для того, чтобы узнать,
+  // сколько времени займёт установка, и успешно
+  // или нет она завершилась.
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
