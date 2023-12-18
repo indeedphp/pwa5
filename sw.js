f82362efa1ef7ddb8423c716124bfed5f7b161fd
indeedphp <ntsiti@gmail.com>
2023-12-18 22:11:07 +0600

    ttt

--- sw.js
+++ sw.js
@@ -8,9 +8,20 @@ const assetUrls = [
 self.addEventListener('install', async event => {
  const cache = await caches.open(staticCacheName)
   await cache.addAll(assetUrls)
-
 })
 
 self.addEventListener('activate', event => {
 console.log('Service worker acte')
 })
+
+self.addEventListener('fetch', event => {
+  console.log('fetch', event.request.url)
+  event.respondWith(cacheFirst(event.request))
+})
+
+async function cacheFirst(request){
+
+const cached = await caches.match(request)
+return cached ?? await fetch (request)
+}
+
